95638a94f0978e3e1b5eba20c4edfef816e4a855
Jay Patel <106821114+jaydevops27@users.noreply.github.com>
2025-09-02 15:34:05 -0400

    Update sendmail.sh

--- sendmail.sh
+++ sendmail.sh
@@ -1,31 +1,23 @@
-attachment_path="/home/user/k8_health_report/k8s_health_report_b01-prd-tfb-prd-w2.html"
-
-# Create proper MIME email
-temp_email="/tmp/k8s_html_email_$$.eml"
-
-cat > "$temp_email" << EOF
-To: $recipient
-From: $sender
-Subject: $subject
+# Create MIME formatted body that mail command can handle
+email_body=$(cat << EOF
 MIME-Version: 1.0
-Content-Type: multipart/mixed; boundary="htmlboundary123"
+Content-Type: multipart/alternative; boundary="boundary123"
 
---htmlboundary123
+This is a multi-part message in MIME format.
+
+--boundary123
+Content-Type: text/plain; charset=UTF-8
+
+Please view this email in an HTML-capable email client to see the formatted report.
+
+--boundary123
 Content-Type: text/html; charset=UTF-8
 
 $content
 
---htmlboundary123
-Content-Type: application/octet-stream
-Content-Disposition: attachment; filename="$(basename "$attachment_path")"
-Content-Transfer-Encoding: base64
-
-$(base64 "$attachment_path")
---htmlboundary123--
+--boundary123--
 EOF
+)
 
-# Send using sendmail
-/usr/sbin/sendmail "$recipient" < "$temp_email"
-
-# Clean up
-rm "$temp_email"
+# Use your working mail command with MIME formatted body
+echo -e "$email_body" | mail -s "$subject" -r "$sender" -a "$attachment_path" "$recipient"

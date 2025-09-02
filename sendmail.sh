4752a6d59f56d2a2a9ad676008c0e040d14724c5
Jay Patel <106821114+jaydevops27@users.noreply.github.com>
2025-09-02 15:11:32 -0400

    Update sendmail.sh

--- sendmail.sh
+++ sendmail.sh
@@ -1,4 +1,12 @@
-echo -e "$content" | mail -s "$subject" -r "$sender" \
-                         -a "Content-Type: text/html; charset=UTF-8" \
-                         -a "/home/user/k8_health_report/k8s_health_report_b01-prd-tfb-prd-w2.html" \
-                         "$recipient"
+temp_file="/tmp/html_email_$$.txt"
+cat > "$temp_file" << EOF
+MIME-Version: 1.0
+Content-Type: text/html; charset=UTF-8
+
+$content
+EOF
+
+# Use your working command with the MIME file
+echo -e "$(cat $temp_file)" | mail -s "$subject" -r "$sender" \
+                                  -a "/home/user/k8_health_report/k8s_health_report_b01-prd-tfb-prd-w2.html" \
+                                  "$recipient"
